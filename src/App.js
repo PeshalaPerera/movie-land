eaa0fd5aee9324946b28d33451e06348c41d940e
Peshala Perera <78086607+PeshalaPerera@users.noreply.github.com>
2024-06-02 18:35:04 +0530

    Revert " Refactor App component, add search functionality, and update movie c…" (#2)
    This reverts commit 5519bc53e12c6af033042b3dda69d5fa1e22451f.

--- src/App.js
+++ src/App.js
@@ -1,54 +1,25 @@
-import { useEffect, useState } from "react";
-import "./App.css";
-import SearchIcon from "./search.svg";
-import MovieCard from "./MovieCard";
+import logo from './logo.svg';
+import './App.css';
 
-const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=9216cb3f";
-
-const App = () => {
-  const [movies, setMovies] = useState([]);
-  const [searchTerm, setSearchTerms] = useState([]);
-
-  const searchMovies = async (title) => {
-    const response = await fetch(`${API_URL}&s=${title}`);
-    const data = await response.json();
-
-    setMovies(data.Search);
-  };
-  useEffect(() => {
-    searchMovies("Princess");
-  }, []);
-
+function App() {
   return (
-    <div className="app">
-      <h1>MovieLand</h1>
-      <div className="search">
-        <input
-          type="text"
-          placeholder="Search..."
-          value={searchTerm}
-          onChange={(e) => setSearchTerms(e.target.value)}
-        />
-        <img
-          src={SearchIcon}
-          alt="search"
-          onClick={() => searchMovies(searchTerm)}
-        />
-      </div>
-
-      {movies?.length > 0 ? (
-        <div className="container">
-          {movies.map((movie) => (
-            <MovieCard movie={movie} />
-          ))}
-        </div>
-      ) : (
-        <div className="empty">
-          <h2>No movies found</h2>
-        </div>
-      )}
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
+}
 
 export default App;
